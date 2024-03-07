42eff494c7b63b53efd0af33ea2666372da73620
Abreham <abrehamdessalegn02@gmail.com>
2024-03-07 16:35:16 +0000

    changing int file

--- inigers.c
+++ inigers.c
@@ -9,11 +9,8 @@
 int printnumber(int i)
 {
 	int val = i, temp = 0, j = 0, k;
-	char *str1;
-	if (i == NULL)
-	{
-		return (0);
-	}
+	char c;
+
 	if (val < 0)
 	{
 		_putchar('-');
@@ -22,24 +19,30 @@ int printnumber(int i)
 	while (val > 10)
 	{
 		temp = val % 10;
-		str1[j] = (char)temp;
 		val = val - temp;
 		val = (int)(val / 10);
 		if (val < 10)
 		{
-			j++;
-			str1[j] = (char)val;
 			break;
 		}
 		j++;
 	}
-	k = lenstring(str1);
-	j = k;
-	k--;
-	while (k >= 0)
+	val = i;
+	k = j;
+	while (j >= 0)
 	{
-		_putchar(str[k]);
-		k--;
+		temp = (int)val / (10 ^ j);
+		if (temp < 10)
+		{
+			c = (char)temp;
+			_putchar(c);
+			j--;
+			continue;
+		}
+		temp = temp % 10;
+		c = (char)temp;
+		_putchar(c);
+		j--;
 	}
-	return (j);
+	return (k);
 }
